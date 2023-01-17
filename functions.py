d349a3b5127c3c33fcc7698736e4ffbfde22be0d
andr00sama <122212181+andr00sama@users.noreply.github.com>
2023-01-17 07:11:40 -0400

    Update functions.py
    removed unnecessary repetition of getTokentxns in order to speed up run duration

--- functions.py
+++ functions.py
@@ -1,24 +1,25 @@
+from datetime import datetime
+import requests, json
+
 def getCurrentBlock():
-    from datetime import datetime
-    import requests, json
     timestamp = str(round(datetime.timestamp(datetime.now())))
     link = "https://api.etherscan.io/api?module=block&action=getblocknobytime&timestamp=" + timestamp + "&closest=before&apikey=3JGAJUKSIMFXWNJWDB4X9QY5CWYP8P262A"
     dictionary = json.loads(requests.get(link).text)
     return dictionary.get("result")
 
 def getTokentxns(contract):
-    import requests, json
     currentBlock = getCurrentBlock()
-    link = "https://api.etherscan.io/api?module=account&action=tokentx&address=" + str(contract) + "&startblock=" + str(int(currentBlock)-500) +"&endblock=" + str(currentBlock)+ "&apikey=3JGAJUKSIMFXWNJWDB4X9QY5CWYP8P262A"
+    link = "https://api.etherscan.io/api?module=account&action=tokentx&address=" + str(contract) + "&startblock=" + str(int(currentBlock)-5) +"&endblock=" + str(currentBlock)+ "&apikey=3JGAJUKSIMFXWNJWDB4X9QY5CWYP8P262A"
     return json.loads(requests.get(link).text)
 
 def getPrice(contract):
-    swap1 = getTokentxns(contract).get("result")[0]
+    tokentxns = getTokentxns(contract).get("result")
+    swap1 = tokentxns[0]
     divisor = 1000000000000000000
     if swap1.get("tokenSymbol") == "USDC":
         divisor = 1000000
     amount1 = int(swap1.get("value"))/divisor
-    swap2 = getTokentxns(contract).get("result")[1]
+    swap2 = tokentxns[1]
     divisor = 1000000000000000000
     if swap2.get("tokenSymbol") == "USDC":
         divisor = 1000000
